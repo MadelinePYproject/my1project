5d0679be06ba961a18358557bbea35a817c0a17f
Admin <vladkovalenko1104@gmail.com>
2024-12-03 21:44:29 +0200

    l2 HOME TASK

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-num = 12345
-reversed_num =(int(str(num)[::-1]))
-print(reversed_num)
+
+
+num = input("Введіть 4-х значне число")
+num = int(num)
+print(num // 1000)
+print(num // 100 % 10)
+print(num // 10 % 10)
+print(num % 10)
+
+
+
+
 
