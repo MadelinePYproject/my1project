a5df362c49352a33589229265f0e1d7474fb7831
Admin <vladkovalenko1104@gmail.com>
2024-12-03 23:17:46 +0200

    addHW

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-number = int(input("5 значне число"))
 
-reversed_number = 0
-reversed_number += (number % 10) * 10000
-number = number // 10
-reversed_number += (number % 10) * 1000
-number = number // 10
-reversed_number += (number % 10) * 100
-number = number // 10
-reversed_number += (number % 10) * 10
-number = number // 10
-
-reversed_number += number % 10
-print(reversed_number)
 
 
 
