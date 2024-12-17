42aad45264d2cbe4da854a650d0b77e9a35eca67
Admin <vladkovalenko1104@gmail.com>
2024-12-17 18:25:36 +0200

    l5 HOME TASK

--- l5.py
+++ l5.py
@@ -1,5 +1,46 @@
-my_list = [0, 1, 7, 2, 4, 8]
-if my_list:
-    index_sum = sum([my_list[idx]for idx in range(0, len(my_list),2)])
-    result = index_sum * my_list[-1]
-print(result)
+import string
+def generate_hashtag(text):
+    if not text.strip():
+        return None
+    clean_text = text.translate(str.maketrans('', '', string.punctuation))
+    words = clean_text.split()
+
+
+    if not words:
+        return None
+
+
+    hashtag = '#' + ''.join(word.capitalize() for word in words)
+    return hashtag[:140]
+print(generate_hashtag('Python Community'))  # #PythonCommunity
+print(generate_hashtag('i like python community!'))  # #ILikePythonCommunity
+print(generate_hashtag('Should, I. subscribe? Yes!'))  # #ShouldISubscribeYes
+
+
+
+while True:
+
+    num_1 = float(input("Ввести число: "))
+    num_2 = float(input("Ввести число: "))
+    operation = input("Ввести действие(+, -, *, /) ")
+    if operation == "+" :
+        result = num_1 + num_2
+        print("Результат:", result)
+    elif  operation == "-":
+        result = num_1 - num_2
+        print("Результат:", result)
+    elif operation == "*" :
+        result = num_1 * num_2
+        print("Результат:", result)
+    elif operation == "/" :
+        if num_2 == 0 :
+            print("Ошибочка:")
+        else:
+            result = num_1 / num_2
+            print("Результат:", result)
+    else:
+        print("Корявая операция")
+    continue_calc = input("Бажаєте продовжити? (y/n): ").lower()
+    if continue_calc != "y":
+        print("Thx for using me, BB!")
+        break
