ebe06cdbcba14079c59be3e76c65b43ef356dcf6
Admin <vladkovalenko1104@gmail.com>
2024-12-17 14:24:26 +0200

    l4 HOME TASK

--- l4.py
+++ l4.py
@@ -10,6 +10,6 @@ print(result)
 
 import random
 list1 = [random.randint(0,100) for i in range(random.randint(3, 10))]
-list2 = [list1[0]],[list1[2]],[list1[-2]]
+list2 = list((list1[0], list1[2], list1[-2]))
 print('list1', list1)
 print('list2', list2)
