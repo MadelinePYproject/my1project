195628e009abb476d129761da7e424e636662149
Admin <vladkovalenko1104@gmail.com>
2024-12-22 23:09:22 +0200

    6.2

--- l6.py
+++ l6.py
@@ -10,4 +10,16 @@ input_data = input("Vvedite diapazon cherez defis: ")
 result = get_characters_range(input_data)
 print(result)
 
+def conver_seconds(seconds):
+    if seconds < 0 or seconds > 8640000:
+        return "shos pishlo ne tydu"
+    seconds_per_minute = 60
+    seconds_per_hour = 3600
+    seconds_per_day = 86400
+    days, seconds = divmod(seconds, seconds_per_day)
+    hour, seconds = divmod(seconds, seconds_per_hour)
+    minutes, seconds = divmod(seconds, seconds_per_minute)
+    return f"{days} {'День' if days == 1 else 'Дней'},{str(hour).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+user_input = int(input("Vvedite kl-vo secund : "))
+print(conver_seconds(user_input))
 
