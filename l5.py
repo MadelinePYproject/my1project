7a6c66d080e2a7383638b3015d1c02fe123dd3ea
Admin <vladkovalenko1104@gmail.com>
2024-12-22 22:59:21 +0200

    l5.1 HW

--- l5.py
+++ l5.py
@@ -44,3 +44,22 @@ while True:
     if continue_calc != "y":
         print("Thx for using me, BB!")
         break
+
+import string
+import keyword
+
+user_input = input('Vvedite ryadok: ')
+variable_name = True
+if user_input[0].isdigit():
+    variable_name =  False
+elif any(char.isupper() for char in user_input):
+    variable_name = False
+elif "" in user_input:
+    variable_name = False
+elif any(char in string.punctuation for char in user_input)and "_" not in user_input:
+    variable_name = False
+elif user_input in keyword.kwlist:
+    variable_name = False
+elif "__" in user_input:
+    variable_name = False
+print(variable_name)
