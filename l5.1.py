e9638448f4039f3a99cc94c93653b0ed8c66fd3f
Admin <vladkovalenko1104@gmail.com>
2024-12-23 21:48:12 +0200

    l.5.1

--- l5.1.py
+++ l5.1.py
@@ -1,10 +1,10 @@
+import string
+import keyword
 user_input = input('Vvedite ryadok: ')
 variable_name = True
 if user_input[0].isdigit():
     variable_name =  False
 elif any(char.isupper() for char in user_input):
-    variable_name = False
-elif "" in user_input:
     variable_name = False
 elif any(char in string.punctuation for char in user_input)and "_" not in user_input:
     variable_name = False
